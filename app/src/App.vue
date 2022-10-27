c8d6d9c1ceee635cfbaf30f15866102867348348
islem143 <djennaneislempro@gmail.com>
2022-10-27 17:28:00 +0100

    add vue-sakai theme

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,31 +1,159 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div :class="containerClass" @click="onWrapperClick">
+        <AppTopBar @menu-toggle="onMenuToggle" />
+        <div class="layout-sidebar" @click="onSidebarClick">
+            <AppMenu :model="menu" @menuitem-click="onMenuItemClick" />
+        </div>
+
+        <div class="layout-main-container">
+            <div class="layout-main">
+                <router-view />
+            </div>
+            <AppFooter />
+        </div>
+
+		<AppConfig :layoutMode="layoutMode" @layout-change="onLayoutChange" />
+        <transition name="layout-mask">
+            <div class="layout-mask p-component-overlay" v-if="mobileMenuActive"></div>
+        </transition>
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import AppTopBar from './AppTopbar.vue';
+import AppMenu from './AppMenu.vue';
+import AppConfig from './AppConfig.vue';
+import AppFooter from './AppFooter.vue';
+
+export default {
+    emits: ['change-theme'],
+    data() {
+        return {
+            layoutMode: 'static',
+            staticMenuInactive: false,
+            overlayMenuActive: false,
+            mobileMenuActive: false,
+            menu : [
+                {
+                    label: 'Home',
+                    items: [{
+                        label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/'
+                    }]
+                },
+				
+            
+			
+                
+              
+            ]
+        }
+    },
+    watch: {
+        $route() {
+            this.menuActive = false;
+            this.$toast.removeAllGroups();
+        }
+    },
+    methods: {
+        onWrapperClick() {
+            if (!this.menuClick) {
+                this.overlayMenuActive = false;
+                this.mobileMenuActive = false;
+            }
+
+            this.menuClick = false;
+        },
+        onMenuToggle() {
+            this.menuClick = true;
+
+            if (this.isDesktop()) {
+                if (this.layoutMode === 'overlay') {
+					if(this.mobileMenuActive === true) {
+						this.overlayMenuActive = true;
+					}
+
+                    this.overlayMenuActive = !this.overlayMenuActive;
+					this.mobileMenuActive = false;
+                }
+                else if (this.layoutMode === 'static') {
+                    this.staticMenuInactive = !this.staticMenuInactive;
+                }
+            }
+            else {
+                this.mobileMenuActive = !this.mobileMenuActive;
+            }
+
+            event.preventDefault();
+        },
+        onSidebarClick() {
+            this.menuClick = true;
+        },
+        onMenuItemClick(event) {
+            if (event.item && !event.item.items) {
+                this.overlayMenuActive = false;
+                this.mobileMenuActive = false;
+            }
+        },
+		onLayoutChange(layoutMode) {
+			this.layoutMode = layoutMode;
+		},
+        addClass(element, className) {
+            if (element.classList)
+                element.classList.add(className);
+            else
+                element.className += ' ' + className;
+        },
+        removeClass(element, className) {
+            if (element.classList)
+                element.classList.remove(className);
+            else
+                element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        },
+        isDesktop() {
+            return window.innerWidth >= 992;
+        },
+        isSidebarVisible() {
+            if (this.isDesktop()) {
+                if (this.layoutMode === 'static')
+                    return !this.staticMenuInactive;
+                else if (this.layoutMode === 'overlay')
+                    return this.overlayMenuActive;
+            }
+
+            return true;
+        }
+    },
+    computed: {
+        containerClass() {
+            return ['layout-wrapper', {
+                'layout-overlay': this.layoutMode === 'overlay',
+                'layout-static': this.layoutMode === 'static',
+                'layout-static-sidebar-inactive': this.staticMenuInactive && this.layoutMode === 'static',
+                'layout-overlay-sidebar-active': this.overlayMenuActive && this.layoutMode === 'overlay',
+                'layout-mobile-sidebar-active': this.mobileMenuActive,
+				'p-input-filled': this.$primevue.config.inputStyle === 'filled',
+				'p-ripple-disabled': this.$primevue.config.ripple === false
+            }];
+        },
+        logo() {
+            return (this.$appState.darkTheme) ? "images/logo-white.svg" : "images/logo.svg";
+        }
+    },
+    beforeUpdate() {
+        if (this.mobileMenuActive)
+            this.addClass(document.body, 'body-overflow-hidden');
+        else
+            this.removeClass(document.body, 'body-overflow-hidden');
+    },
+    components: {
+        'AppTopBar': AppTopBar,
+        'AppMenu': AppMenu,
+        'AppConfig': AppConfig,
+        'AppFooter': AppFooter,
+    }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style lang="scss">
+@import './App.scss';
 </style>
