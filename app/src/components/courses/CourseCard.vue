671db5fbca57ae612a6168f01b84b44e7271b868
islem143 <djennaneislempro@gmail.com>
2022-11-11 08:38:28 +0100

    fix course card ui and add media type

--- app/src/components/courses/CourseCard.vue
+++ app/src/components/courses/CourseCard.vue
@@ -1,15 +1,23 @@
 <template>
   <div
     @click="courseDetail"
-    class="flex pa-7 justify-content-between cursor-pointer card p-fuild h-8rem transition-all transition-duration-200 hover:bg-gray-100 shadow-4 hover:shadow-6"
+    class="pa-7 cursor-pointer p-5 m-4 border-round  transition-all transition-duration-200 hover:bg-gray-100 shadow-4 hover:shadow-6"
   >
-    <div>
-      <a href="" class="text-xl font-bold text-900">{{ course.title }}</a>
-      <p class="mt-2">{{ course.description }}</p>
+    <div class="flex justify-content-between">
+      <div>
+        <a href="" class="text-xl font-bold text-900">{{ course.title }}</a>
+        <p class="mt-2">Description: {{ course.description }}</p>
+      </div>
+
+      <!-- <div>
+        <Button label="Start" icon="pi pi-arrow-right" iconPos="right" />
+      </div> -->
     </div>
-    <div>
-      <Button label="Start" icon="pi pi-arrow-right" iconPos="right" />
-    </div>
+    <h6>Courses:</h6>
+    <p v-for="media in course.media">
+      <i :class="icons[media.type]+' mr-3 mt-2 border-1 p-1 surface-200 text-900'" style="font-size: 1rem"> </i>
+      <a class="text-900 text-lg hover:underline" :href="'http://localhost:8081/' + media.url">{{ media.name }}</a>
+    </p>
   </div>
 </template>
 
@@ -24,11 +32,23 @@ export default {
       type: Number,
     },
   },
+  data() {
+    return {
+      icons: {
+        video: "pi pi-video",
+        image: "pi pi-image",
+        file: "pi pi-file",
+      },
+    };
+  },
   methods: {
     courseDetail() {
       this.$router.push({
         name: "course-detail",
-        params: { moduleId: this.$route.params.moduleId, courseId: this.course.id },
+        params: {
+          moduleId: this.$route.params.moduleId,
+          courseId: this.course.id,
+        },
       });
     },
   },
